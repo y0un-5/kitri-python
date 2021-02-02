b13e9b62352c5b86498b5ef612ff016c6fb6166e
y0un-5 <78009442+y0un-5@users.noreply.github.com>
2021-02-02 13:42:24 +0900

    2021-02-02
    First Commit

--- Class/birth1.py
+++ Class/birth1.py
@@ -29,6 +29,51 @@ def birthday(ssn):
 
     #print(f"생년월일: {year}년{month}월 {day}일")
 
+def age(ssn):
+    import datetime
+    if not isinstance(ssn, str):
+        return None
+    elif len(ssn) != 14:
+        return None
+    elif not (ssn[0:6].isdigit() and ssn[6] == '-' and ssn[7:14].isdigit()):
+        return None
+    uid = int(ssn[7])
+    c18xx, c19xx, c20xx = (9, 0), (1, 2, 5, 6), (3, 4, 7, 8)
+
+    if uid in c19xx:
+        c = 1900
+    if uid in c20xx:
+        c = 2000
+    if uid in c18xx:
+        c = 1800
+
+    year = c + int(ssn[0:2])
+    month = int(ssn[2:4])
+    day = int(ssn[4:6])
+    
+    today = int(datetime.date.today().strftime("%Y%m%d"))
+    birth = year * 10000 + month * 100 + day
+    age = (today - int(birth)) // 10000
+    
+    return age
+
+def gender(ssn):
+    if not isinstance(ssn, str):
+        return None
+    elif len(ssn) != 14:
+        return None
+    elif not (ssn[0:6].isdigit() and ssn[6] == '-' and ssn[7:14].isdigit()):
+        return None
+    uid = int(ssn[7])
+    male, female = (1, 3, 5, 7, 9), (2, 4, 6, 8)
+    
+    if uid in male :
+        gender = "남자"
+    elif uid in female :
+        gender = "여자"
+
+    return gender
+
 
 """
 ## 주민등록번호 : 800512-1133555
@@ -50,22 +95,9 @@ def birthday(ssn):
 ## 위 조건이 만족되지 않으면 입력받지 않도록 설정 
 """
 
-"""
-def gender(ssn):
-    
-    uid = int(ssn[7])
-    male, female = (1, 3, 5, 7, 9), (2, 4, 6, 8)
-    
-    if uid in male :
-        gender = "남자"
-    elif uid in female :
-        gender = "여자"
-
-    print(f"성별: {gender}")
-"""
-
-
 ssn  = input('주민등록번호 : ')
 print(birthday(ssn))
+print(age(ssn))
+print(gender(ssn))
 #gender(ssn)
 #파일 자체 출력시
